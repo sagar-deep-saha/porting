10d5c4227ed83ab9c4e39c15c16282e9e71e508f
Sagar Deep Saha <sagarsaha@outlook.in>
2024-11-25 11:18:59 +0530

    update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
             <img src={foot_logo} className="arka2" alt="arka" />
           </a>
           <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-            Copyright© 2024; Designed & Developed by Sagar Deep Saha & Arkajyoti Roy
+            Copyright© 2024; Designed & Developed by Sagar Deep Saha & <a href="https://arka-port.vercel.app/" style={{textDecoration:"none",color:"black"}}>Arkajyoti Roy</a> 
           </p>
         </div>
       </footer>
