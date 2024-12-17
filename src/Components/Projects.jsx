d18eeddb576d2e515214919f3a5e80cc90274f9e
Sagar Deep Saha <sagarsaha@outlook.in>
2024-12-17 19:43:38 +0530

    update Projects.jsx

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -194,6 +194,21 @@ const Projects = () => {
                     Visit
                   </button>
                 </a>
+
+                <a
+                  href="https://diploma.arkajyoti.shop/"
+                  target="_blank"
+                >
+                  {" "}
+                  <button
+                    data-ripple-light="true"
+                    type="button"
+                    style={{ backgroundColor: "#e3261d" }}
+                    className="select-none rounded-lg bg-blue-500 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-blue-500/20 transition-all hover:shadow-lg hover:shadow-blue-500/40 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+                  >
+                    Visit
+                  </button>
+                </a>
               </div>
             </div>
           </SwiperSlide>
